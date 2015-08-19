27ba51dab06c14ed40b04e560d6aa26e38b3720d
MKS-students <mks.students@hackreactor.com>
2015-08-19 15:28:47 -0500

    create helper functions

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -25,17 +25,77 @@ exports.initialize = function(pathsObj){
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(){
+exports.readListOfUrls = function(cb){
+  fs.readFile(exports.paths.list, function (err,data) {
+    if (err) throw err;
+    data = '' + data;
+    console.log("exports.readListOfURLs data:", data);
+    var siteList = [];
+    for (var i = 0; i < data.length; i++) {
+      if(data[i] === "\n") {
+        siteList.push(data.slice(0,i)); // start here after lunch
+        console.log("siteList", siteList);
+        data = data.slice(i+1);
+        i = 0;
+      }
+    }
+    siteList.push(data);
+    console.log("final sitelist", siteList);
+    cb(siteList);
+  } )
 };
 
-exports.isUrlInList = function(){
+exports.isUrlInList = function(url, cb){
+  var siteList = exports.readListOfUrls(function (read) {
+    return read;
+  });
+  cb( _.contains(siteList, url) );
 };
 
-exports.addUrlToList = function(){
+exports.addUrlToList = function(url, cb){
+  if (exports.isUrlInList(url, function (x) {
+    return x;
+  })) {
+    cb(false);
+  } else {
+    fs.appendFile(exports.paths.list, '\n' + url, function (err) {
+      if (err) throw err;
+      cb(true);
+    })
+  }
+
 };
 
-exports.isUrlArchived = function(){
+exports.isUrlArchived = function(url, cb){
+  var archiveList = fs.readdir(exports.paths.archivedSites, function(err,archiveList) {
+    if (err) throw err;
+    return archiveList;
+  });
+  cb(_.contains(archiveList,url));
 };
 
-exports.downloadUrls = function(){
+exports.downloadUrls = function(urlArray){
+  for (var i = 0; i < urlArray.length; i++) {
+    // if(exports.isUrlArchived(urlArray[i], function (x) {
+    //   return x;
+    // })) {
+    //   continue;
+    // }
+    fs.open(exports.paths.archivedSites + "/" + urlArray[i], 'w', function (err, fd) {
+      if (err) throw err;
+      var buffer = 'i am the buffer, replace me LATTAAAAA'
+      fs.write(fd, buffer , 0, buffer.length, function (err, written, buffer) {
+        if(err) throw err;
+        return;
+      })
+    })
+  }
 };
+
+
+
+
+
+
+
+
